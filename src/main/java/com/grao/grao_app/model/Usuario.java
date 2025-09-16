240bd34dbf9f7639f19ac4c06bdf23a843519955
Raphael Willian <raphaelwillian.cardososilva@gmail.com>
2025-09-15 21:29:41 -0300

    Configurações iniciais de Usuarios

---
+++ src/main/java/com/grao/grao_app/model/Usuario.java
@@ -0,0 +1,18 @@
+package com.grao.grao_app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+//Porque é uma entidade (Usuários)
+@Entity
+public class usuarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+
+}
---
+++ src/main/java/com/grao/grao_app/repository/UsuarioRepository.java
@@ -0,0 +1,4 @@
+package com.grao.grao_app.repository;
+
+public interface UsuarioRepository {
+}
